c10097ef3d74c5a6886b619a183d537336217a87
scy <120@qq.com>
2018-11-15 22:52:12 +0800

    zs

--- testGit/src/main/java/com/scy/MainDemo.java
+++ testGit/src/main/java/com/scy/MainDemo.java
@@ -2,6 +2,6 @@ package com.scy;
 
 public class MainDemo {
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("helloaaaaaaaaaa");
     }
 }
